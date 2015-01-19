3d95abf40d0869ecfd0ae0c643720bf7b026b51b
Elimarie <iglitzi@fullsail.edu>
2015-01-18 20:42:26 -0500

    Changes
    I had to change everything I haven’t slept since yesterday to make this
    website look as my ADT class design. Now is finally working.

---
+++ site/js/main.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+
+$(document).ready(function(){
+	$('#nav').slicknav();
+	// fitvids jquery for do video responsive 
+	$("html").niceScroll({
+		cursorcolor:"#01a394",
+		cursorwidth: "8px"
+	});
+        
+});
